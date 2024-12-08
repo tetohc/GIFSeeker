174ce9c92ea5b23869cbd50a05165b15e84b5e90
Jerry Hurtado Castillo <jerryalonso01@gmail.com>
2024-12-08 11:37:30 -0600

    Initial commit of completed project

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
-    const api_key = 'YOUR_API_KEY';
-    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}=${category}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    const apiKey = 'YOUR_API_KEY';
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${category}&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
     const response = await fetch(apiUrl);
     const { data } = await response.json();
 
